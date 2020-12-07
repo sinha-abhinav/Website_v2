8d7486ed5c7b95dcc7e64577489688da5683e180
Vaibhav Sinha <vaibhavsinhayash@gmail.com>
2020-12-08 02:01:25 +0530

    initial commit

---
+++ js/brock bastian/static-context.js
@@ -0,0 +1,502 @@
+Static = window.Static || {};
+Static.SQUARESPACE_CONTEXT = {
+  facebookAppId: '314192535267336',
+  facebookApiVersion: 'v6.0',
+  rollups: {
+    'squarespace-announcement-bar': {
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/announcement-bar-b8a8eff740f9ed8386726-min.en-US.js',
+    },
+    'squarespace-audio-player': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/audio-player-52bdd87acccf92a19703e60e09c654bd-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/audio-player-48de9bd562bb77c5750f9-min.en-US.js',
+    },
+    'squarespace-blog-collection-list': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/blog-collection-list-d41d8cd98f00b204e9800998ecf8427e-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/blog-collection-list-422e0a6039ffd794c3adb-min.en-US.js',
+    },
+    'squarespace-calendar-block-renderer': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/calendar-block-renderer-1e8a762808391e4b0bd8945da50793ac-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/calendar-block-renderer-ec1ec37c02aa2c5a4ffc6-min.en-US.js',
+    },
+    'squarespace-chartjs-helpers': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/chartjs-helpers-9935a41d63cf08ca108505d288c1712e-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/chartjs-helpers-4153b2d7ef2af44268fbc-min.en-US.js',
+    },
+    'squarespace-comments': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/comments-1637eb4ee93bed59528921026e024bf4-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/comments-32762ab89d23599036901-min.en-US.js',
+    },
+    'squarespace-commerce-cart': {
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/commerce-cart-b3529a4784b1353fcd13c-min.en-US.js',
+    },
+    'squarespace-dialog': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/dialog-afeb96ba160fb642a2d98c892a956065-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/dialog-b7538784ac72489e342f0-min.en-US.js',
+    },
+    'squarespace-events-collection': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/events-collection-1e8a762808391e4b0bd8945da50793ac-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/events-collection-8dd6d30a4c81d4b3c0b9c-min.en-US.js',
+    },
+    'squarespace-form-rendering-utils': {
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/form-rendering-utils-09b401b041fed371114cf-min.en-US.js',
+    },
+    'squarespace-forms': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/forms-d21f3e517c32f4caba9f220beea0666d-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/forms-e6940275c6eb7f40cc2c2-min.en-US.js',
+    },
+    'squarespace-gallery-collection-list': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/gallery-collection-list-d41d8cd98f00b204e9800998ecf8427e-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/gallery-collection-list-bf8fb32966c4e9e8d9949-min.en-US.js',
+    },
+    'squarespace-image-zoom': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/image-zoom-72b0ab7796582588032aa6472e2e2f14-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/image-zoom-cebb0feb61c6f0366b12c-min.en-US.js',
+    },
+    'squarespace-pinterest': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/pinterest-d41d8cd98f00b204e9800998ecf8427e-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/pinterest-e2914c9ccae5ae5c212ce-min.en-US.js',
+    },
+    'squarespace-popup-overlay': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/popup-overlay-e984c5797664760d617b07bce15392a1-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/popup-overlay-6d8ef30c7116ff408bcf0-min.en-US.js',
+    },
+    'squarespace-product-quick-view': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/product-quick-view-3fa8fd4df0755e7a7878e60166ca3d61-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/product-quick-view-8aa07e677b9951da1edda-min.en-US.js',
+    },
+    'squarespace-products-collection-item-v2': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/products-collection-item-v2-72b0ab7796582588032aa6472e2e2f14-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/products-collection-item-v2-7d22aa378ad95382ba1e1-min.en-US.js',
+    },
+    'squarespace-products-collection-list-v2': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/products-collection-list-v2-72b0ab7796582588032aa6472e2e2f14-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/products-collection-list-v2-0bf177d7025ced6d33c65-min.en-US.js',
+    },
+    'squarespace-search-page': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/search-page-207da8872118254c0a795bf9b187c205-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/search-page-6a5641bb704ad1a891ad4-min.en-US.js',
+    },
+    'squarespace-search-preview': {
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/search-preview-4758846ba2f75c441347c-min.en-US.js',
+    },
+    'squarespace-share-buttons': {
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/share-buttons-b12f3ca1f74e143c0c645-min.en-US.js',
+    },
+    'squarespace-simple-liking': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/simple-liking-9ef41bf7ba753d65ec1acf18e093b88a-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/simple-liking-739561dd7d5175896afb7-min.en-US.js',
+    },
+    'squarespace-social-buttons': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/social-buttons-bf7788a87c794b73afd9d5c49f72f4f3-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/social-buttons-28a395563b686201a49a4-min.en-US.js',
+    },
+    'squarespace-tourdates': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/tourdates-d41d8cd98f00b204e9800998ecf8427e-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/tourdates-1aaf83ff13b1203851bbb-min.en-US.js',
+    },
+    'squarespace-website-overlays-manager': {
+      css:
+        '//assets.squarespace.com/universal/styles-compressed/website-overlays-manager-68df56a8135961401cbd590171d6b04a-min.css',
+      js:
+        '//assets.squarespace.com/universal/scripts-compressed/website-overlays-manager-0a0af8b87cccc73835dd3-min.en-US.js',
+    },
+  },
+  pageType: 1,
+  website: {
+    id: '57d2f8668419c276f91f3b04',
+    identifier: 'brock-bastian',
+    websiteType: 1,
+    contentModifiedOn: 1582211820101,
+    cloneable: false,
+    hasBeenCloneable: false,
+    siteStatus: {},
+    language: 'en-US',
+    timeZone: 'Australia/Melbourne',
+    machineTimeZoneOffset: 36000000,
+    timeZoneOffset: 36000000,
+    timeZoneAbbr: 'AEST',
+    siteTitle: 'Brock Bastian',
+    fullSiteTitle: 'Brock Bastian',
+    siteTagLine:
+      'Melbourne School of Psychological Sciences University of Melbourne',
+    siteDescription:
+      '<p>Brock Bastian, Melbourne School of Psychological Sciences</p>',
+    shareButtonOptions: {
+      '4': true,
+      '1': true,
+      '7': true,
+      '2': true,
+      '3': true,
+      '8': true,
+      '6': true,
+    },
+    authenticUrl: 'http://www.brockbastian.com',
+    internalUrl: 'http://brock-bastian.squarespace.com',
+    baseUrl: 'http://www.brockbastian.com',
+    primaryDomain: 'www.brockbastian.com',
+    sslSetting: 1,
+    isHstsEnabled: false,
+    typekitId: '',
+    statsMigrated: false,
+    imageMetadataProcessingEnabled: false,
+    screenshotId:
+      '1d864910fee5838136a7be71ecf87644ed495605f246af556069d9a68c4af68d',
+    showOwnerLogin: false,
+  },
+  websiteSettings: {
+    id: '57d2f8668419c276f91f3b07',
+    websiteId: '57d2f8668419c276f91f3b04',
+    subjects: [],
+    country: 'US',
+    state: 'CA',
+    simpleLikingEnabled: true,
+    mobileInfoBarSettings: {
+      isContactEmailEnabled: false,
+      isContactPhoneNumberEnabled: false,
+      isLocationEnabled: false,
+      isBusinessHoursEnabled: false,
+    },
+    commentLikesAllowed: true,
+    commentAnonAllowed: true,
+    commentThreaded: true,
+    commentApprovalRequired: false,
+    commentAvatarsOn: true,
+    commentSortType: 2,
+    commentFlagThreshold: 0,
+    commentFlagsAllowed: true,
+    commentEnableByDefault: true,
+    commentDisableAfterDaysDefault: 0,
+    disqusShortname: '',
+    commentsEnabled: false,
+    storeSettings: {
+      returnPolicy: null,
+      termsOfService: null,
+      privacyPolicy: null,
+      expressCheckout: false,
+      continueShoppingLinkUrl: '/',
+      useLightCart: false,
+      showNoteField: false,
+      shippingCountryDefaultValue: 'US',
+      billToShippingDefaultValue: false,
+      showShippingPhoneNumber: true,
+      isShippingPhoneRequired: false,
+      showBillingPhoneNumber: true,
+      isBillingPhoneRequired: false,
+      currenciesSupported: [
+        'CHF',
+        'HKD',
+        'MXN',
+        'EUR',
+        'DKK',
+        'USD',
+        'CAD',
+        'MYR',
+        'NOK',
+        'THB',
+        'AUD',
+        'SGD',
+        'ILS',
+        'PLN',
+        'GBP',
+        'CZK',
+        'SEK',
+        'NZD',
+        'PHP',
+        'RUB',
+      ],
+      defaultCurrency: 'USD',
+      selectedCurrency: 'USD',
+      measurementStandard: 1,
+      showCustomCheckoutForm: false,
+      enableMailingListOptInByDefault: true,
+      sameAsRetailLocation: false,
+      merchandisingSettings: {
+        scarcityEnabledOnProductItems: false,
+        scarcityEnabledOnProductBlocks: false,
+        scarcityMessageType: 'DEFAULT_SCARCITY_MESSAGE',
+        scarcityThreshold: 10,
+        multipleQuantityAllowedForServices: true,
+        restockNotificationsEnabled: false,
+        restockNotificationsMailingListSignUpEnabled: false,
+        relatedProductsEnabled: false,
+        relatedProductsOrdering: 'random',
+        soldOutVariantsDropdownDisabled: false,
+        productComposerOptedIn: false,
+      },
+      isLive: false,
+      multipleQuantityAllowedForServices: true,
+    },
+    useEscapeKeyToLogin: true,
+    trialAssistantEnabled: true,
+    ssBadgeType: 1,
+    ssBadgePosition: 4,
+    ssBadgeVisibility: 1,
+    ssBadgeDevices: 1,
+    pinterestOverlayOptions: { mode: 'disabled' },
+    ampEnabled: false,
+  },
+  cookieSettings: {
+    isCookieBannerEnabled: false,
+    isRestrictiveCookiePolicyEnabled: false,
+    isRestrictiveCookiePolicyAbsolute: false,
+    cookieBannerText: '',
+    cookieBannerTheme: '',
+    cookieBannerVariant: '',
+    cookieBannerPosition: '',
+    cookieBannerCtaVariant: '',
+    cookieBannerCtaText: '',
+  },
+  websiteCloneable: false,
+  collection: {
+    title: 'Home',
+    id: '57d2fc8fbebafbc1a4a7487a',
+    fullUrl: '/',
+    type: 1,
+  },
+  subscribed: false,
+  appDomain: 'squarespace.com',
+  templateTweakable: true,
+  tweakJSON: {
+    'aspect-ratio': 'Auto',
+    contentBgColor: 'rgba(255,255,255,1)',
+    'first-index-image-fullscreen': 'false',
+    'fixed-header': 'true',
+    footerBgColor: 'rgba(60,52,47,1)',
+    'gallery-arrow-style': 'No Background',
+    'gallery-auto-crop': 'true',
+    'gallery-autoplay': 'false',
+    'gallery-design': 'Slideshow',
+    'gallery-info-overlay': 'Show on Hover',
+    'gallery-loop': 'false',
+    'gallery-navigation': 'Bullets',
+    'gallery-show-arrows': 'true',
+    galleryArrowBackground: 'rgba(34,34,34,1)',
+    galleryArrowColor: 'rgba(255,255,255,1)',
+    galleryAutoplaySpeed: '3',
+    galleryCircleColor: 'rgba(255,255,255,1)',
+    galleryInfoBackground: 'rgba(0, 0, 0, .7)',
+    galleryThumbnailSize: '100px',
+    gridSize: '350px',
+    gridSpacing: '20px',
+    headerBgColor: 'rgba(60,52,47,0.9)',
+    imageOverlayColor: 'rgba(46,46,46,0.3)',
+    'index-image-height': 'Half',
+    'parallax-scrolling': 'true',
+    'product-gallery-auto-crop': 'true',
+    'product-image-auto-crop': 'true',
+    'title--description-alignment': 'Center',
+    'title--description-position': 'Over Image',
+    titleBgColor: 'rgba(60,52,47,0.9)',
+    'transparent-header': 'true',
+    'tweak-v1-related-products-title-spacing': '50px',
+  },
+  templateId: '515c7bd0e4b054dae3fcf003',
+  templateVersion: '7',
+  pageFeatures: [1, 2, 4],
+  gmRenderKey: 'QUl6YVN5Q0JUUk9xNkx1dkZfSUUxcjQ2LVQ0QWVUU1YtMGQ3bXk4',
+  betaFeatureFlags: [
+    'domain_deletion_via_registrar_service',
+    'dg_downloads_from_fastly',
+    'seven_one_fonts_panel_targeting',
+    'donations_customer_accounts',
+    'commerce_instagram_product_checkout_links',
+    'domain_info_via_registrar_service',
+    'gallery_settings_71',
+    'ORDERS-SERVICE-check-digital-good-access-with-service',
+    'domain_locking_via_registrar_service',
+    'domains_allow_async_gsuite',
+    'seven_one_portfolio_hover_layouts',
+    'trust_arc_on_config',
+    'gallery_captions_71',
+    'blog_event_item_settings',
+    'onboarding_wizard_seven_one',
+    'domains_transfer_flow_improvements',
+    'ORDER_SERVICE-submit-subscription-order-through-service',
+    'commerce_activation_experiment_add_payment_processor_card',
+    'domain_registration_via_registrar_service',
+    'domains_use_new_domain_connect_strategy',
+    'ORDER_SERVICE-submit-reoccurring-subscription-order-through-service',
+    'demo_content_improvement',
+    'domains_transfer_flow_hide_preface',
+    'generic_iframe_loader_for_campaigns',
+    'seven_one_image_overlay_opacity',
+    'campaigns_single_opt_in',
+    'commerce_minimum_order_amount',
+    'new_billing_system',
+    'local_listings',
+    'filter_features',
+    'global_animations',
+    'commerce_product_composer_all_types',
+    'ORDERS-SERVICE-reset-digital-goods-access-with-service',
+    'ORDERS-SERVICE-refund-order-using-workflow',
+    'newsletter_block_captcha',
+    'seven_one_theme_mapper_v3',
+    'commerce_subscription_order_delay',
+    'ORDERS-SERVICE-cancel-order-using-workflow',
+    'commerce_restock_notifications',
+  ],
+  yuiEliminationExperimentList: [
+    {
+      name: 'statsMigrationJobWidget-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'ContributionConfirmed-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'false',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'TextPusher-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'MenuItemWithProgress-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'imageProcJobWidget-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'QuantityChangePreview-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'CompositeModel-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'false',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'HasPusherMixin-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'INACTIVE',
+    },
+    {
+      name: 'ProviderList-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'MediaTracker-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'false',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'pushJobWidget-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'internal-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'BillingPanel-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'false',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'PopupOverlayEditor-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+    {
+      name: 'CoverPagePicker-enabled',
+      experimentType: 'AB_TEST',
+      variant: 'true',
+      containsError: false,
+      status: 'ACTIVE',
+    },
+  ],
+  impersonatedSession: false,
+  tzData: {
+    zones: [[600, 'AV', 'AE%sT', null]],
+    rules: {
+      AV: [
+        [1995, 2005, null, 'Mar', 'lastSun', '2:00s', '0', 'S'],
+        [2001, 2007, null, 'Oct', 'lastSun', '2:00s', '1:00', 'D'],
+        [2006, 'only', null, 'Apr', 'Sun>=1', '2:00s', '0', 'S'],
+        [2007, 'only', null, 'Mar', 'lastSun', '2:00s', '0', 'S'],
+        [2008, 'max', null, 'Apr', 'Sun>=1', '2:00s', '0', 'S'],
+        [2008, 'max', null, 'Oct', 'Sun>=1', '2:00s', '1:00', 'D'],
+      ],
+    },
+  },
+};
